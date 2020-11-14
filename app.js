60fa45b38d34184a0aa16bb9087a0f9d03094034
sanyam.a <sanyam.a@neodove.com>
2020-11-15 00:06:56 +0530

    implemented redis cache

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const express = require('express'),
     axios = require('axios'),
     redis = require('redis'),
     responseTime = require('response-time'),
-    app = express();
+    app = express(),
+    { promisify } = require('util');
 
 app.use(responseTime());
+
+const client = redis.createClient();
+
+const getFromCache = promisify(client.get).bind(client);
+const setToCache = promisify(client.set).bind(client);
+
+app.get('/get-rockets', async (req, res, next) => {
+    try {
+        const cachedData = await getFromCache('get-data');
+        if (cachedData) {
+            console.log('Getting Data from Cache!!');
+            res.send(JSON.parse(cachedData));
+            return;
+        }
+        const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+        await setToCache('get-data', JSON.stringify(response.data), 'EX', 100);
+        res.send(response.data);
+    } catch (err) {
+        res.send(err.message);
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server Started..');
+});
